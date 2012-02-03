ac5b5cb4c72f64b4a19ac8654fc517cb8d931601
Stuart Sierra <mail@stuartsierra.com>
2012-02-03 17:09:08 -0500

    Make Clojure build script copy production CSS files

--- src/clj/build.clj
+++ src/clj/build.clj
@@ -1,8 +1,9 @@
 (ns build
   (:require [cljs.closure :as closure]
-            [clojure.java.browse :as browse]))
+            [clojure.java.browse :as browse]
+            [clojure.java.io :as io]))
 
-(def default-options
+(def production-options
   {:output-dir "out/production"
    :output-to "out/production/org-html-slides.js"
    :optimizations :advanced})
@@ -13,14 +14,13 @@
    :optimizations :whitespace
    :pretty-print true})
 
-
 (defn build [mode]
-  (closure/build
-   "src/cljs"
-   (if (= mode :development)
-     development-options
-     default-options)))
-
-
-(defn browse []
-  (browse/browse-url ""))
+  (closure/build "src/cljs"
+                 (if (= mode :development)
+                   development-options
+                   default-options))
+  (when (= mode :production)
+    (doseq [f (filter #(and (.isFile %)
+                            (.. % getName (endsWith ".css")))
+                      (file-seq (io/file "src/css")))]
+      (io/copy f (io/file "out/production" (.getName f))))))
