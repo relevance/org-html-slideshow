b3a22fe992a822da9c096de6dea4867bd8f17cc7
Stuart Sierra and Alex Redington <pair@thinkrelevance.com>
2012-02-03 14:36:52 -0500

    Add Lein project.clj

---
+++ project.clj
@@ -0,0 +1,4 @@
+(defproject org-html-slides "0.0.1-SNAPSHOT"
+  :dependencies [[org.clojure/clojure "1.4.0-alpha5"]
+                 [org.clojure/clojurescript "0.0-971"]]
+  :library-path "lib/jars")
