f4966c97b1319b16f77ea6eb48e35b729261aa32
Stuart Sierra <mail@stuartsierra.com>
2012-08-14 15:24:33 -0400

    Update project.clj for Leiningen 2

--- project.clj
+++ project.clj
@@ -1,7 +1,6 @@
 (defproject org-html-slides "0.0.1-SNAPSHOT"
+  :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.4.0-alpha5"]
                  [org.clojure/clojurescript "0.0-971"]]
   :library-path "lib/jars"
-  :source-path "src/clj"
-  :extra-classpath-dirs ["lib/domina/src/cljs"
-                         "lib/one/src/lib/cljs"])
+  :source-paths ["src/clj" "lib/domina/src/cljs" "lib/one/src/lib/cljs"])
