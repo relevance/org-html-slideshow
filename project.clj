e2ddd47eefa9c479cd59cda2c68c78bfbc5eebde
Stuart Sierra <mail@stuartsierra.com>
2013-09-27 14:49:01 -0400

    Switch build to using lein-cljsbuild
    Eliminate Clojure sources just used for building.
    
    One consequence: there is no build-time step to copy CSS files from
    src/css to production/

--- project.clj
+++ project.clj
@@ -1,6 +1,20 @@
 (defproject org-html-slides "0.0.1-SNAPSHOT"
   :min-lein-version "2.0.0"
-  :dependencies [[org.clojure/clojure "1.4.0-alpha5"]
-                 [org.clojure/clojurescript "0.0-971"]]
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.clojure/clojurescript "0.0-1896"]]
+  :plugins [[lein-cljsbuild "0.3.3"]]
   :library-path "lib/jars"
-  :source-paths ["src/clj" "lib/domina/src/cljs" "lib/one/src/lib/cljs"])
+  :source-paths ["lib/domina/src/cljs" "lib/one/src/lib/cljs"]
+  :cljsbuild {:builds
+              {:development
+               {:source-paths ["src/cljs"]
+                :compiler {:output-to "out/development/org-html-slideshow.js"
+                           :output-dir "out/development"
+                           :optimizations :whitespace
+                           :pretty-print true}}
+               :production
+               {:source-paths ["src/cljs"]
+                :compiler {:output-to "production/org-html-slideshow.js"
+                           :output-dir "production"
+                           :optimizations :advanced
+                           :pretty-print false}}}})
